689e3b9806263d2d78a1db4c6c0066f3cdc62bc4
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-09-14 10:19:53 -0300

    [FEATURE]criando teste para criar usuario

--- test/userTest.test.ts
+++ test/userTest.test.ts
@@ -1,10 +1,53 @@
 import app from '../src/app';
 import supertest from 'supertest';
+import { faker } from '@faker-js/faker';
+import client from '../src/dbStrategy/db';
 
-describe('POST /users',async ()=>{
-    it('Create new user',async ()=>{
-        const result = await supertest(app).post("/users/create").send()
+beforeAll(async ()=>{
+    await client.$executeRaw`TRUNCATE TABLE user`
+})
 
-        expect(result.status).toBe(201)
+describe('POST /users', ()=>{
+    const password = faker.internet.password(8)
+    const userData = {
+            email:faker.internet.email(),
+            password:password,
+            repeatPassword: password
+        }
+
+    it('Create new user', async()=>{
+        const result = await supertest(app).post("/users/signup").send(userData);
+        const createdUser = await client.user.findUnique({where:{email:userData.email}});
+
+        expect(result.status).toBe(201);
+        expect(createdUser).not.toBeNull();
+    })
+    it('Exist email conflict',async ()=>{
+        const result = await supertest(app).post("/users/signup").send(userData);
+        expect(result.status).toBe(409)
+    })
+
+    it('Login with user', async()=> {
+        const result = await supertest(app).post('/users/signin').send({email:userData.email,password:userData.password})
+
+        expect(result.body).toBeInstanceOf(Object)
+    })
+
+    it('Login with wrong password/email',async()=>{
+        const wrongPassword = await supertest(app).post('/users/signin').send({email:userData.email,password:faker.internet.password()});
+        const wrongEmail = await supertest(app).post('/users/signin').send({email:faker.internet.email(),password:userData.password});
+
+        expect(wrongPassword.status && wrongEmail.status).toBe(409);
+    })
+
+    it('Empty data',async()=>{
+       const signinEmpty =  await supertest(app).post('/users/signin').send('');
+       const signupEmpty =  await supertest(app).post('/users/signin').send('');
+
+       expect(signinEmpty.status && signupEmpty.status).toBe(422)
     })
 })
+
+afterAll(async() => {
+    await client.$disconnect();
+});
